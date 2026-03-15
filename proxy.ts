e3dfd2877b3dee58fc07310b0690c09fd8a62171
Lina <chim.lina25@kit.edu.kh>
2026-03-16 00:43:28 +0700

    initial

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request: { cookies: { get: (arg0: string) => any; }; nextUrl: { pathname: string; }; url: string | URL | undefined; }) {
+export function proxy(request: { cookies: { get: (arg0: string) => any; }; nextUrl: { pathname: string; }; url: string | URL | undefined; }) {
   try {
     // Example logic: Redirect unauthenticated users
     const token = request.cookies.get('auth-token');
